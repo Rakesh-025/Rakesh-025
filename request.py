b8444df7b83993e7b4d669f02e181d33e6e7e36d
Rakesh-025 <107669272+Rakesh-025@users.noreply.github.com>
2022-07-08 23:30:36 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'Maintenance_cost':50000, 'Marketing_cost':9000, 'Profit_Margin':6500,'Level_of_Course': 2,'Duration_of_coaching_in_Hours': 120})
+r = requests.post(url,json={'Maintenance_cost':1000, 'Marketing_cost':100, 'Profit_Margin':100,'Level_of_Course': 2,'Duration_of_coaching_in_Hours': 100})
 
 print(r.json())
